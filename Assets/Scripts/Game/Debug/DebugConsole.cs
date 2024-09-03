22cc2235a82484b06d7819654ea9c81d38750f32
Michael Souster <michael.souster@live.co.uk>
2024-09-03 20:28:35 +0100

    Extra 4 - Premium debug console

--- Assets/Scripts/Game/Debug/DebugConsole.cs
+++ Assets/Scripts/Game/Debug/DebugConsole.cs
@@ -1,24 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Linq;
+using UnityEngine;
 
 public class DebugConsole : MonoBehaviour
 {
-    private bool showConsole = false;
-    private string _consoleInput;
-
     enum DisplayType
     {
-        None,
+        History,
         Help,
         Autocomplete
     }
+    enum LogType
+    {
+        Command,
+        Output,
+        Error,
+    }
+    private static Dictionary<LogType, Color> logColors = new Dictionary<LogType, Color>()
+    {
+        { LogType.Output, Color.cyan },
+        { LogType.Error, Color.red },
+    };
 
-    private DisplayType displayType = DisplayType.None;
+    struct HistoryItem
+    {
+        public string message;
+        public LogType type;
+    }
+
+    private static GUIStyle _logStyle;
+
+    private bool _showConsole = false;
+    private string _consoleInput;
+
+    private DisplayType _displayType;
+
+    private const int MAX_HISTORY_LENGTH = 5;
+    private int _historyCurrentIndex;
+    private Queue<HistoryItem> _history = new Queue<HistoryItem>();
+    private HistoryItem[] _browsableHistory
+        => _history.Where(i => i.type == LogType.Command).ToArray();
 
     private void Awake()
     {
+        new DebugCommand("?", "Lists all available debug commands.", "?", () =>
+        {
+            _displayType = DisplayType.Help;
+        });
+
         new DebugCommand("toggle_fov", "Toggles the FOV parameter on/off", "toggle_fov", () =>
         {
             print("toggle fov acrivated");
@@ -31,31 +61,34 @@ public class DebugConsole : MonoBehaviour
             Globals.AVAILABLE_RESOURCES[GameManager.instance.gamePlayersParameters.myPlayerId][InGameResource.Gold].AddAmount(x);
             EventManager.TriggerEvent("UpdateResourceTexts");
         });
-
-        new DebugCommand("?", "Lists all available debug commands.", "?", () =>
-        {
-            displayType = DisplayType.Help;
-        });
+        _displayType = DisplayType.History;
     }
 
     private void OnEnable()
     {
-        EventManager.AddListener("<Input>ShowDebugConsole", OnShowDebugConsole);
+        EventManager.AddListener("<Input>ShowDebugConsole", _OnShowDebugConsole);
     }
 
     private void OnDisable()
     {
-        EventManager.RemoveListener("<Input>ShowDebugConsole", OnShowDebugConsole);
+        EventManager.RemoveListener("<Input>ShowDebugConsole", _OnShowDebugConsole);
     }
 
-    private void OnShowDebugConsole()
+    private void _OnShowDebugConsole()
     {
-        showConsole = true;
+        _showConsole = true;
+        EventManager.TriggerEvent("PausedGame");
     }
 
     private void OnGUI()
     {
-        if (showConsole)
+        if (_logStyle == null)
+        {
+            _logStyle = new GUIStyle(GUI.skin.label);
+            _logStyle.fontSize = 12;
+        }
+
+        if (_showConsole)
         {
             // add fake boxes in the background to increase the opacity
             GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
@@ -67,14 +100,16 @@ public class DebugConsole : MonoBehaviour
             // show log area
             float y = 24;
             GUI.Box(new Rect(0, y, Screen.width, Screen.height - 24), "");
-            if (displayType == DisplayType.Help)
-                ShowHelp(y);
-            else if (displayType == DisplayType.Autocomplete)
-                ShowAutocomplete(y, newInput);
+            if (_displayType == DisplayType.Help)
+                _ShowHelp(y);
+            else if (_displayType == DisplayType.Autocomplete)
+                _ShowAutocomplete(y, newInput);
+            else
+                _ShowHistory(y);
 
-            // reset display state to "none" if input changes
-            if (displayType != DisplayType.None && _consoleInput.Length != newInput.Length)
-                displayType = DisplayType.None;
+            // reset display state to "history" if input changes
+            if (_displayType != DisplayType.History && _consoleInput.Length != newInput.Length)
+                _displayType = DisplayType.History;
 
             // update input variable
             _consoleInput = newInput;
@@ -83,19 +118,83 @@ public class DebugConsole : MonoBehaviour
             Event e = Event.current;
             if (e.isKey)
             {
-                if (e.keyCode == KeyCode.Return && _consoleInput.Length > 0)
-                {
+                if (e.keyCode == KeyCode.Tab)
+                    _displayType = DisplayType.Autocomplete;
+                else if (e.keyCode == KeyCode.Return && _consoleInput.Length > 0)
                     _OnReturn();
-                }
                 else if (e.keyCode == KeyCode.Escape)
                 {
-                    showConsole = false;
+                    _showConsole = false;
+                    EventManager.TriggerEvent("ResumedGame");
                 }
-                else if (e.keyCode == KeyCode.Tab)
+                else if (e.keyCode == KeyCode.UpArrow)
                 {
-                    displayType = DisplayType.Autocomplete;
+                    _historyCurrentIndex--;
+                    if (_historyCurrentIndex < 0)
+                        _historyCurrentIndex = 0;
+
+                    _consoleInput = _browsableHistory[_historyCurrentIndex].message;
+                }
+                else if (e.keyCode == KeyCode.DownArrow)
+                {
+                    HistoryItem[] browsableHistory = _browsableHistory;
+                    if (_historyCurrentIndex == browsableHistory.Length) return;
+
+                    _historyCurrentIndex++;
+                    if (_historyCurrentIndex == browsableHistory.Length)
+                    {
+                        _consoleInput = "";
+                    }
+                    else
+                    {
+                        _consoleInput = browsableHistory[_historyCurrentIndex].message;
+                    }
                 }
             }
+        }
+    }
+
+    private void _ShowHelp(float y)
+    {
+        foreach (DebugCommandBase command in DebugCommandBase.DebugCommands.Values)
+        {
+            GUI.Label(
+                new Rect(2, y, Screen.width, 20),
+                $"{command.Format} - {command.Description}",
+                _logStyle
+            );
+            y += 16;
+        }
+    }
+
+    private void _ShowAutocomplete(float y, string newInput)
+    {
+        IEnumerable<string> autocompleteCommands =
+                            DebugCommandBase.DebugCommands.Keys
+                            .Where(k => k.StartsWith(newInput.ToLower()));
+        foreach (string k in autocompleteCommands)
+        {
+            DebugCommandBase c = DebugCommandBase.DebugCommands[k];
+            GUI.Label(
+                new Rect(2, y, Screen.width, 20),
+                $"{c.Format} - {c.Description}",
+                _logStyle
+            );
+            y += 16;
+        }
+    }
+
+    private void _ShowHistory(float y)
+    {
+        foreach (HistoryItem x in _history)
+        {
+            Color c = GUI.color;
+            Color c2;
+            if (logColors.TryGetValue(x.type, out c2))
+                GUI.color = c2;
+            GUI.Label(new Rect(2, y, Screen.width, 20), x.message, _logStyle);
+            y += 16;
+            GUI.color = c;
         }
     }
 
@@ -103,17 +202,20 @@ public class DebugConsole : MonoBehaviour
     {
         _HandleConsoleInput();
         _consoleInput = "";
+        _historyCurrentIndex = _browsableHistory.Length;
     }
 
     private void _HandleConsoleInput()
     {
         // parse input
-        string[] inputParts = _consoleInput.Split(" ");
+        string[] inputParts = _consoleInput.Split(' ');
         string mainKeyword = inputParts[0];
         // check against available commands
         DebugCommandBase command;
         if (DebugCommandBase.DebugCommands.TryGetValue(mainKeyword.ToLower(), out command))
         {
+            _LogMessage(_consoleInput);
+
             // try to invoke command if it exists
             if (command is DebugCommand dc)
                 dc.Invoke();
@@ -121,38 +223,69 @@ public class DebugConsole : MonoBehaviour
             {
                 if (inputParts.Length < 2)
                 {
-                    Debug.LogError("Missing parameter!");
+                    _LogError("Missing parameter!");
                     return;
                 }
-                if (command is DebugCommand<int> dcInt)
+
+                if (command is DebugCommand<string> dcString)
+                {
+                    dcString.Invoke(inputParts[1]);
+                }
+                else if (command is DebugCommand<int> dcInt)
                 {
                     int i;
                     if (int.TryParse(inputParts[1], out i))
                         dcInt.Invoke(i);
+                    else
+                    {
+                        _LogError($"'{command.Id}' requires an int parameter!");
+                        return;
+                    }
                 }
+                else if (command is DebugCommand<float> dcFloat)
+                {
+                    float f;
+                    if (float.TryParse(inputParts[1], out f))
+                        dcFloat.Invoke(f);
+                    else
+                    {
+                        _LogError($"'{command.Id}' requires a float parameter!");
+                        return;
+                    }
+                }
+                //else if (command is DebugCommand<string, int> dcStringInt)
+                //{
+                //    int i;
+                //    if (int.TryParse(inputParts[2], out i))
+                //        dcStringInt.Invoke(inputParts[1], i);
+                //    else
+                //    {
+                //        _LogError($"'{command.Id}' requires a string and an int parameter!");
+                //        return;
+                //    }
+                //}
             }
         }
-    }
-
-    private void ShowHelp(float y)
-    {
-        foreach (DebugCommandBase command in DebugCommandBase.DebugCommands.Values)
+        else
         {
-            GUI.Label(new Rect(2, y, Screen.width, 20), $"{command.Format} - {command.Description}");
-            y += 16;
+            _LogError("Unkwown command!");
         }
     }
 
-    private void ShowAutocomplete(float y, string newInput)
+    private void _LogOutput(string msg)
     {
-        IEnumerable<string> autocompleteCommands =DebugCommandBase.DebugCommands.Keys
-            .Where(k => k.StartsWith(newInput.ToLower()));
+        _LogMessage($">> {msg}", LogType.Output);
+    }
 
-        foreach (string k in autocompleteCommands)
-        {
-            DebugCommandBase c = DebugCommandBase.DebugCommands[k];
-            GUI.Label(new Rect(2, y, Screen.width, 20), $"{c.Format} - {c.Description}");
-            y += 16;
-        }
+    private void _LogError(string msg)
+    {
+        _LogMessage(msg, LogType.Error);
+    }
+
+    private void _LogMessage(string msg, LogType t = LogType.Command)
+    {
+        _history.Enqueue(new HistoryItem { message = msg, type = t });
+        if (_history.Count > MAX_HISTORY_LENGTH)
+            _history.Dequeue();
     }
 }
